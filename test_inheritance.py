608894e14e831e33080dcedaed0570af44b17699
Christine <christine.apimo27@gmail,com>
2017-03-07 19:28:36 +0300

    abstraction

---
+++ test_inheritance.py
@@ -0,0 +1,8 @@
+import unittest
+from inheritance import withdraw
+
+class inheritance(unittest.Testcase):
+	def test_balance(self):
+		self.assertEqual(self.balance, 500)
+if__name__=="__main__"
+unittest.main()
