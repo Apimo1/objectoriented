0e70f89b37a1e3b4147ef239301a776a36a46865
Christine <christine.apimo27@gmail,com>
2017-03-07 18:38:46 +0300

    creating a class with attributes

---
+++ attribute.py
@@ -0,0 +1,21 @@
+class BankAccount(object):
+	"""A bank accoount with the following attributes
+	deposit
+	withdraw
+	balance"""
+	def __init__(self,name,balance=500.0):
+		"""Returns name of the account holderthe minimum amount in the account"""
+		self.name=name
+		self.balance=balance
+
+	def deposit(self,amount):
+		"""Returns integral amount after the deposit"""
+		self.balance +=amount
+		return self.balance
+
+	def withdraw(self,amount):
+		"""Returns intergral amount after withdrawal"""
+		if amount>self.balance:
+				raise RuntimeError("Withdrawal amount greater than account balance")
+		self.balance -=amount
+		return self.balance	
